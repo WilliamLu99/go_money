dc3a7c65a0da99dc403ef9873512a44306d0cdcd
William Lu <william@tamedfox.ca>
2024-11-29 22:51:14 -0500

    Styling

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
     has_many :group_memberships, dependent: :destroy
-    has_many :users, :through => :group_memberships, dependent: :destroy
+    has_many :users, through: :group_memberships, dependent: :destroy
     has_many :money_transactions, class_name: "Transaction", dependent: :destroy
 end
